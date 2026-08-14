251c28012aa200be1ad858ad58b136adbdd059db
Doha Abdelrahman <dohaabdo757@gmail.com>
2026-08-14 22:09:35 +0000

    delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
         <ExperienceSection />
         <EducationSection />
         <CertificationsSection />
-        <GitHubSection />
         <ContactSection />
       </main>
       
