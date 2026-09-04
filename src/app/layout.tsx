5cac508df4c2dc3a1d96e23ba300cb73a6474cd3
Doha Abdelrahman <dohaabdo757@gmail.com>
2026-09-04 21:29:18 +0000

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,9 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "Doha Adelrahman" }],
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📊</text></svg>",
+    icon: "/workspaces/Doha/src/app/icon.png",
   },
+
   openGraph: {
     title: "Doha Adelrahman | Data Scientist",
     description:
